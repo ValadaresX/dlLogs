1e3308befc26b5770c4f08a378ab9e538b309b24
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2023-06-10 16:50:15 -0300

    1.01
    Adicionado consectários e legibilidade de funcoes.

--- copy_logs.py
+++ copy_logs.py
@@ -14,17 +14,18 @@ from tqdm import tqdm
 logging.getLogger('urllib3').setLevel(logging.INFO)
 logging.StreamHandler()
 
-
+#  Configuração do diretorios
 URL_BASE = 'https://storage.googleapis.com/wowarenalogs-log-files-prod/'
 BASE_DIR = os.getcwd()
 KEYS_FILE = Path(BASE_DIR) / 'keys.txt'
 LOGS_DIR = Path(BASE_DIR) / 'logs'
 
+# Define o tamanho máximo do trecho do arquivo de log a ser lido
+MAX_BYTES = 50000000
 
-MAX_BYTES = 50000000  # Define o tamanho máximo do trecho do arquivo de log a ser lido
 
-
-def get_xml() -> requests.Response:  # Busca o XML no servidor remoto
+# Busca o XML no servidor remoto
+def get_xml() -> requests.Response:
     response = requests.get(URL_BASE)
     response.raise_for_status()
     return response
@@ -52,6 +53,7 @@ def download_log_file(log_key: str, response: requests.Response, progress_bar: t
         update_keys_file(log_key)
 
 
+#  Faz o download de um arquivo e salva no disco.
 def download_file(response: requests.Response, log_file_path: str, progress_bar: tqdm, bytes_total: int) -> None:
     with open(log_file_path, 'wb') as file:
         for chunk in response.iter_content(1024):
@@ -59,6 +61,7 @@ def download_file(response: requests.Response, log_file_path: str, progress_bar:
             progress_bar.update(len(chunk))
 
 
+#  Fixa a codificação de caracteres de um arquivo de log.
 def fix_encoding(log_file_path: Path) -> None:
     with log_file_path.open('rb') as file:
         log_bytes = file.read(MAX_BYTES)
@@ -79,8 +82,9 @@ def is_log_file_complete(file_path: Path, response: requests.Response) -> bool:
     return file_path.stat().st_size >= expected_size
 
 
+# Verifica se todos os arquivos de log correspondentes a cada chave listada no XML estão presentes no disco.
 def check_logs_on_disk(response: requests.Response) -> None:
-    # Verifica se todos os arquivos de log correspondentes a cada chave listada no XML estão presentes no disco.
+
     keys_xml = get_keys_from_response(response)
     missing_logs = keys_xml - set([f.stem for f in LOGS_DIR.glob('*.txt')])
 
@@ -99,7 +103,8 @@ def update_keys_file(key: str) -> None:
             f.write(key + '\n')
 
 
-def download_new_logs() -> None:  # Baixa novos logs do servidor remoto.
+# Baixa novos logs do servidor remoto.
+def download_new_logs() -> None:
     new_keys = get_new_keys()
     if new_keys:
         print(f'Baixando {len(new_keys)} novos logs...')
@@ -116,26 +121,17 @@ def download_new_logs() -> None:  # Baixa novos logs do servidor remoto.
         print('Todos os logs estão presentes no disco.')
 
 
+# Retorna um conjunto de novas chaves
 def get_new_keys() -> set:
-    """Retorna um conjunto de novas chaves."""
+
     response = get_xml()
     keys_xml = get_keys_from_response(response)
     keys_downloaded = set([f.stem for f in LOGS_DIR.glob('*.txt')])
     return keys_xml - keys_downloaded
 
 
-def main() -> None:
-    """Baixa os logs para as chaves especificadas no servidor remoto."""
-
-    if not LOGS_DIR.exists():
-        LOGS_DIR.mkdir(parents=True)
-
-    print('Baixando o XML...')
-
-    download_new_logs()
-
-
-def run_job():  # Executão do script no intervalo entre 8 a 10 horas
+# Executão do script no intervalo entre 8 a 10 horas
+def run_job():
     random_seconds = random.uniform(8 * 3600, 10 * 3600)
     total_time = int(random_seconds)
     with tqdm(total=total_time, desc="Próxima execução...") as pbar:
@@ -144,6 +140,17 @@ def run_job():  # Executão do script no intervalo entre 8 a 10 horas
             pbar.update(1)
             total_time -= 1
     main()
+
+
+# Baixa os logs para as chaves especificadas no servidor remoto.
+def main() -> None:
+
+    if not LOGS_DIR.exists():
+        LOGS_DIR.mkdir(parents=True)
+
+    print('Baixando o XML...')
+
+    download_new_logs()
 
 
 if __name__ == '__main__':
